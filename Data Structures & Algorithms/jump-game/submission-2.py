f1d0263c7a83207ff1a4266a32e973976d8452d6
aiyuan wang <145239325+specialistway@users.noreply.github.com>
2026-06-01 12:06:46 +0800

    Add: jump-game - submission-2

---
+++ Data Structures & Algorithms/jump-game/submission-2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        goal=len(nums)-1
+        for i in range(len(nums)-2,-1,-1):
+            if i+nums[i]>=goal:
+                goal=i
+        return goal==0
+
+       
+
+        
+#哦那是最大的 感觉还是线性dp

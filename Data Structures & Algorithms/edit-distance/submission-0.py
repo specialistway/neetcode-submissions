87f2f5eaa29a3c52ba94119058ad40022ddb84ba
aiyuan wang <145239325+specialistway@users.noreply.github.com>
2026-08-28 23:28:47 +0800

    Add: edit-distance - submission-0

---
+++ Data Structures & Algorithms/edit-distance/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        n,m=len(word1),len(word2)
+        dp=[[0]*(m+1) for _ in range(n+1)]
+        for i in range(m):
+            dp[n][i]=m-i
+        for i in range(n):
+            dp[i][m]=n-i
+        for i in range(len(word1)-1,-1,-1):
+            for j in range(len(word2)-1,-1,-1):
+                if word1[i]==word2[j]:
+                    dp[i][j]=dp[i+1][j+1]
+                else:
+                    #insert
+                    dp[i][j]=dp[i][j+1]+1
+                    #delete
+                    dp[i][j]=min(dp[i+1][j]+1,dp[i][j])
+                    #replace
+                    dp[i][j]=min(dp[i+1][j+1]+1,dp[i][j])
+        return dp[0][0]
+
+        
